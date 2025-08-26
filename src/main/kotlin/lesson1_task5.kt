25f7e0c80445babeeae7f5e411b212e04b2d1513
Vladislav <prileosky.vladislav25@gmai.com>
2025-08-26 16:33:21 +0300

    KS-1-5-refactor-task-2

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -3,8 +3,8 @@ fun main() {
     val TOTAL_SECONDS : Int = 6480
     val SECONDS_IN_MINUTE = 60
 
-    val hours = TOTAL_SECONDS / (60*60)
-    val secondsAfterHours = TOTAL_SECONDS % (60*60)
+    val hours = TOTAL_SECONDS / (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)
+    val secondsAfterHours = TOTAL_SECONDS % (SECONDS_IN_MINUTE * SECONDS_IN_MINUTE)
     val minutes = secondsAfterHours / SECONDS_IN_MINUTE
     val seconds = secondsAfterHours % SECONDS_IN_MINUTE
 
