13d312cd3dac81d187753710785c4eca804220e1
Vladislav <prileosky.vladislav25@gmai.com>
2025-08-26 13:36:32 +0300

    KS-1-5-refactor-task

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -1,12 +1,12 @@
 fun main() {
 
-    val TOTAL_SECONDS : Int = 6480  //всего секунд
-    val SECONDS_IN_MINUTE = 60 //внес константу
+    val TOTAL_SECONDS : Int = 6480
+    val SECONDS_IN_MINUTE = 60
 
     val hours = TOTAL_SECONDS / (60*60)
-    val secondsAfterHours = TOTAL_SECONDS % (60*60) //сколько секунд после часов
-    val minutes = secondsAfterHours / SECONDS_IN_MINUTE //60 заменил на константу
-    val seconds = secondsAfterHours % SECONDS_IN_MINUTE //так же
+    val secondsAfterHours = TOTAL_SECONDS % (60*60)
+    val minutes = secondsAfterHours / SECONDS_IN_MINUTE
+    val seconds = secondsAfterHours % SECONDS_IN_MINUTE
 
     println("%02d:%02d:%02d".format(hours, minutes, seconds))
 
